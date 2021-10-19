d1c0a3e57c82ca6a7bfff3fd83a0b6f3833b4e75
Dries Hugaerts <dries.hugaerts@live.be>
2021-10-19 14:47:38 +0200

    introduced more resilient exception handling.

--- app.js
+++ app.js
@@ -5,27 +5,29 @@ import { run as mailHealingReport } from './tasks/mail-healing-report';
 import { run as mailOnError } from './tasks/mail-on-error';
 import { run as mailOnFailedJob } from './tasks/mail-on-failed-job';
 
-app.use(bodyParser.json({ type: function(req) { return /^application\/json/.test( req.get('content-type') ); } }));
+app.use(bodyParser.json({
+  type: function(req) {
+    return /^application\/json/.test(req.get('content-type'));
+  }
+}));
 
-app.get('/', function( _, res ) {
+app.get('/', function(_, res) {
   res.send('Hello from delta-producer-report-generator :)');
-} );
+});
 
-app.post('/delta', async function( req, res ) {
+app.post('/delta', async function(req, res) {
   const delta = req.body;
   const inserts = flatten(delta.map(changeSet => changeSet.inserts));
-  if(inserts.length){
-    try {
-      await mailHealingReport(inserts);
-      await mailOnError(inserts);
-      await mailOnFailedJob(inserts);
+  if (inserts.length) {
+    for (const run of [mailHealingReport, mailOnError, mailOnFailedJob]) {
+      try {
+        await run(inserts);
+      } catch (error) {
+        console.error(`There was an error processing ${inserts.join('\n')}`);
+        console.error(error);
+      }
     }
-    catch(error){
-      console.error(`There was an error processing ${inserts.join('\n')}`);
-      console.error(error);
-    }
-  }
-  else {
+  } else {
     console.log('Incoming deltas do not contain anything to be reported, skipping.');
     res.status(204).send();
   }
